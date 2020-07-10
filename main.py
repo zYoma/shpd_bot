c6de4f8cce4676dfd5cbffbc6e369c3501af36bd
Николай Зимин <zyoma@mail.ru>
2020-07-10 11:52:43 +0300

    ver. 1.0

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 from flask import request
 from flask import jsonify
+from sqlalchemy import func
 
-from models import User
+from models import User, Contact
 from config import URL
 from config import TOKEN
 from config import IP
@@ -12,6 +13,9 @@ from config import sslify
 from config import db
 import requests
 import re
+import json
+
+CONTACTS = {}
 
 
 def send_Message(chat_id, text, kb=None):
@@ -22,6 +26,41 @@ def send_Message(chat_id, text, kb=None):
     answer = {'chat_id': chat_id, 'text': text, 'parse_mode': 'Markdown'}
     if kb is not None:
         answer['reply_markup'] = kb_markup
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def edit_Message(chat_id, message_id, text):
+    url = URL + 'editMessageText'
+    answer = {'chat_id': chat_id, 'message_id': message_id,
+              'text': text, 'parse_mode': 'HTML'}
+    r = requests.post(url, json=answer)
+    return r.json()
+
+
+def send_inline_results(id, admin, result):
+    url = URL + 'answerInlineQuery'
+    results = []
+    for contact in result:
+        input_message_content = {
+            'message_text': contact.date, 'parse_mode': 'Markdown'}
+        data = {
+            'type': 'article',
+            'title': contact.date,
+            'id': contact.id,
+            'input_message_content': input_message_content
+        }
+        if admin:
+            date = f'delete__{contact.id}'
+            kb_markup = {'inline_keyboard': [
+                [{'text': 'Удалить', 'callback_data': date}]]}
+            data['reply_markup'] = kb_markup
+        results.append(data)
+
+    answer = {
+        'inline_query_id': id,
+        'results': json.dumps(results),
+    }
     r = requests.post(url, json=answer)
     return r.json()
 
@@ -75,27 +114,88 @@ def send_event(chat_id, input):
         send_Message(chat_id, text=text, kb=1)
 
 
+def search(query):
+    re_start_word = r"(^|\s)%s"
+    text = re.escape(query.lower())
+    date = Contact.query.all()
+    result = [contact for contact in date if re.search(
+        re_start_word % text, str(contact.date.lower()))]
+
+    return result
+
+
+def delete_contact(id):
+    exists_contact = db.session.query(
+        Contact.id).filter_by(id=id).scalar() is not None
+    if exists_contact:
+        contact = Contact.query.filter(Contact.id == id).delete()
+        db.session.commit()
+        return True
+    return False
+
+
+def create_contact(message):
+    contact = Contact(date=message)
+    db.session.add(contact)
+    db.session.commit()
+
+
 @app.route(f'/{TOKEN}/', methods=['POST'])
 def index():
 
     r = request.get_json()
-    if 'callback_query' in r:
-        callback_data = r['callback_query']['data']
-        callback_from_chat_id = r['callback_query']['message']['chat']['id']
-        callback_message_id = r['callback_query']['message']['message_id']
+    write_json(r)
+    # если бот вызван в inlineрежиме
+    if 'inline_query' in r:
+        id = r['inline_query']['id']
+        query = r['inline_query']['query']
+        chat_id = r['inline_query']['from']['id']
+        result = search(query)
+        if result:
+            admin = is_admin(chat_id)
+            send_inline_results(id, admin, result)
+        return jsonify(r)
+
+    elif 'callback_query' in r:
+        callback_data = r['callback_query']['data'].split('__')
+        callback_from_chat_id = r['callback_query']['from']['id']
         first_name = r['callback_query']['from']['first_name']
-        chat_id = callback_from_chat_id
-        message_id = callback_message_id
+        callback_message = r['callback_query'].get('message')
+        if callback_message:
+            callback_message_id = r['callback_query']['message']['message_id']
+            callback_message_text = r['callback_query']['message']['text']
+
+        if callback_data[0] == 'delete':
+            if is_admin(callback_from_chat_id):
+                delete = delete_contact(callback_data[1])
+                text = 'Удалено!' if delete else 'ID не найден.'
+                send_Message(callback_from_chat_id, text=text)
+
+        return jsonify(r)
+
     else:
         chat_id = r['message']['chat']['id']
         first_name = r['message']['from']['first_name']
         message = r['message'].get('text')
+        via_bot = r['message'].get('via_bot')
 
         access = check_user(first_name, chat_id)
         if not access:
             return jsonify(r)
 
         if message:
+            if message == '/exit':
+                CONTACTS.pop(chat_id, None)
+                send_Message(chat_id, text='OK!')
+                return jsonify(r)
+
+            if chat_id in CONTACTS:
+                if CONTACTS[chat_id] == 'adress':
+                    create_contact(message)
+                    CONTACTS.pop(chat_id, None)
+                    send_Message(chat_id, text='Сохранено!')
+                    return jsonify(r)
+
             if re.search(r'add', message):
                 # Добавление пользователя.
                 if is_admin(chat_id):
@@ -113,13 +213,18 @@ def index():
             elif message == 'Аварии RX':
                 send_event(chat_id, input='input_rx.txt')
 
-            else:
-                text = '*МТС-ШПД-БОТ*\nДля получения информации по авариям на сети воспользуйтесь соответствующими клавишами.\nСписок аварий обновляется раз в 10 минут.\nДля поиска контактов УК/старших по дому отправте адрес.'
-                send_Message(chat_id, text=text, kb=1)
-                # d = User.query.all()
-                # print(d)
+            elif message == 'Добавить контакт':
+                CONTACTS[chat_id] = 'adress'
+                send_Message(
+                    chat_id, text='Ввежите данные по следующему шаблону:\n*Адрес дома* _пробел_ *контактные данные*\nЕсли не хотите добавлять данные нажмите /exit')
 
-        write_json(r)
+            else:
+                if via_bot:
+                    return jsonify(r)
+
+                text = '*МТС-ШПД-БОТ*\nДля получения информации по авариям воспользуйтесь соответствующими клавишами.\nСписок аварий обновляется раз в *10 минут*.\nДля поиска контактов начните вводить сообщение:\n*@MTS_SHPD_Bot* _Адрес который ищите_'
+                send_Message(chat_id, text=text, kb=1)
+
         return jsonify(r)
 
 
--- models.py
+++ models.py
@@ -10,3 +10,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.user_hash
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Text, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        super(Contact, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<Contact %r>' % self.date
